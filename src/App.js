f8250981e5cfa402e22ec0f9912c557fafdaa01c
alziyan <ansari.alziyan@gmail.com>
2024-07-13 02:11:27 +0530

    resposive

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       <h1 className="text-center text-3xl font-bold mb-2">Battle of Signs</h1>
       <Board />
       <button
-        className="absolute top-4 right-4 px-4 py-2 bg-gray-50 text-black border border-blue-200 rounded hover:bg-gray-200"
+        className="absolute  top-1 right-1 px-2 py-1  md:top-4 md:right-4 md:px-4 md:py-2 bg-gray-50 text-black border border-blue-200 rounded hover:bg-gray-200"
         onClick={handleShowInstructions}
       >
         Help
