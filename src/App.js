0113499c7beabbc0b5095a02c9fa612b25f059fb
alziyan <ansari.alziyan@gmail.com>
2024-07-13 01:59:16 +0530

    added Help button

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       <h1 className="text-center text-3xl font-bold mb-2">Battle of Signs</h1>
       <Board />
       <button
-        className="absolute top-4 right-4 px-4 py-2 bg-blue-500 text-white rounded"
+        className="absolute top-4 right-4 px-4 py-2 bg-gray-50 text-black border border-blue-200 rounded hover:bg-gray-200"
         onClick={handleShowInstructions}
       >
         Help
